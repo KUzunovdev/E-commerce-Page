d9e48c365edaff584b75df2665a815b7daaa7d09
Ki11akrokBg <kikouzunow@gmail.com>
2023-07-31 14:51:23 +0300

    feat: Create Product Grid component

--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import ProductCart from "./ProductCart"
 
-export default function ProductGrid() {
+const ProductGrid = ({ products }) => {
+ 
+  const initialProductsToShow = 20;
+  const [visibleProducts, setVisibleProducts] = React.useState(products.slice(0, initialProductsToShow));
+
+  
+  const handleLoadMore = () => {
+    const nextProducts = products.slice(visibleProducts.length, visibleProducts.length + initialProductsToShow);
+    setVisibleProducts([...visibleProducts, ...nextProducts]);
+  };
+
   return (
-    <div>ProductGrid</div>
-  )
-}
+    <div className="container mx-auto mt-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+        {visibleProducts.map((product) => (
+          <ProductCart key={product.id} product={product} />
+        ))}
+      </div>
+      {visibleProducts.length < products.length && (
+        <button onClick={handleLoadMore} className="bg-blue-500 text-white p-2 mt-4">
+          Load More
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ProductGrid;
